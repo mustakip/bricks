b321e238c4c05d42577c18b7c021c7bc7ce3f71b
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2019-01-14 21:18:09 +0530

    Removed the unnecessary code

--- ball.js
+++ ball.js
@@ -1,10 +1,5 @@
 class Ball {
   constructor(radius, left, bottom, speed) {
-    // this.width = radius * 2;
-    // this.height = radius * 2;
-    // this.left = left;
-    // this.bottom = bottom;
-    // this.speed = speed;
     this.position = {
       xCoOrdinate: left,
       yCoOrdinate: bottom
@@ -17,34 +12,6 @@ class Ball {
       x: speed,
       y: speed
     };
-    // this.actions = [this.increment, this.decrement];
-    // this.getLeftAction = this.toggle(this.actions);
-    // this.getBottomAction = this.toggle(this.actions);
-    // this.leftAction = this.increment;
-    // this.bottomAction = this.increment;
-  }
-
-  // increment(value) {
-  //   return value + this.speed;
-  // }
-
-  // decrement(value) {
-  //   return value - this.speed;
-  // }
-
-  toggle(list) {
-    let counter = 1;
-    return function() {
-      let index = counter++ % list.length;
-      return list[index];
-    };
-  }
-
-  toggleLeftAction() {
-    this.leftAction = this.getLeftAction();
-  }
-  toggleBottomAction() {
-    this.bottomAction = this.getBottomAction();
   }
 
   move() {
--- game.js
+++ game.js
@@ -3,43 +3,9 @@ class Game {
     this.paddle = paddle;
     this.ball = ball;
     this.wall = wall;
-    // this.height = height;
-    // this.width = width;
     this.gameStatus = "continue";
   }
 
-  // hasColidedWithVerticalWalls() {
-  //   return this.hasColidedWithLeft() || this.hasColidedWithRight();
-  // }
-
-  // hasColidedWithTopOrPaddle() {
-  //   return this.hascolidedWithPaddle() || this.hasColidedWithTop();
-  // }
-  // hasColidedWithTop() {
-  //   return this.ball.bottom + this.ball.height >= this.height;
-  // }
-  // hasColidedWithBottom() {
-  //   return this.ball.bottom <= 0;
-  // }
-  // hasColidedWithRight() {
-  //   return this.ball.left + this.ball.width >= this.width;
-  // }
-  // hasColidedWithLeft() {
-  //   return this.ball.left <= 0;
-  // }
-  // hascolidedWithPaddle() {
-  //   return this.isAtHeightOfPaddle() && this.isOnThePaddle();
-  // }
-  // isAtHeightOfPaddle() {
-  //   let paddleHeight = this.paddle.bottom + this.paddle.height;
-  //   return this.ball.bottom <= paddleHeight;
-  // }
-  // isOnThePaddle() {
-  //   let ballLeft = this.ball.left + this.ball.width / 2;
-  //   let isWithinLeftSide = ballLeft >= this.paddle.left;
-  //   let isWithinRightSide = ballLeft <= this.paddle.left + this.paddle.width;
-  //   return isWithinLeftSide && isWithinRightSide;
-  // }
   isPaddleInsideLeft() {
     return this.paddle.position.left > 0;
   }
@@ -71,17 +37,6 @@ class Game {
     let gameStatus = this.wall.checkBottomWallColision(position);
     this.gameStatus = gameStatus;
   }
-
-  // if (this.hasColidedWithVerticalWalls()) {
-  //   this.ball.toggleLeftAction();
-  // }
-  // if (this.hasColidedWithTopOrPaddle()) {
-  //   this.ball.toggleBottomAction();
-  // }
-  // if (this.hasColidedWithBottom()) {
-  //   this.gameStatus = "over";
-  //   this.ball.toggleBottomAction();
-  // }
 
   checkColision() {
     this.checkBallPaddleColision();
